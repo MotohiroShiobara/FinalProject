0fa36cf4e61d5b14284319c81424e5f398f15151
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-06-26 14:39:57 +0900

    userテーブルの作成

---
+++ src/main/resources/db/migration/V1__CreateUserTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY
+    ,name VARCHAR(50) NOT NULL
+    ,email VARCHAR(255) NOT NULL
+    ,password VARCHAR(255) NOT NULL
+    ,description VARCHAR (255)
+)
