e74898f255735ffa0f0ea9747242fab144a3c24b
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-06-26 11:40:47 +0900

    initial commit

---
+++ src/main/kotlin/com/final/project/Qiitq/QiitqApplication.kt
@@ -0,0 +1,11 @@
+package com.final.project.Qiitq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QiitqApplication
+
+fun main(args: Array<String>) {
+    runApplication<QiitqApplication>(*args)
+}
---
+++ src/test/kotlin/com/final/project/Qiitq/QiitqApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.final.project.Qiitq
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class QiitqApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
