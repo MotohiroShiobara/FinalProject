7b3e72ed3461e3e3c7a02c639b0d2595be7ea80d
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-06-29 13:30:05 +0900

    descriptionをprofileni

--- src/main/kotlin/com/final/project/Qiita/domain/User.kt
+++ src/main/kotlin/com/final/project/Qiita/domain/User.kt
@@ -1,3 +1,3 @@
 package com.final.project.Qiita.domain
 
-data class User(val accountName: String = "", val emailAddress: String = "", val password: String = "", val description: String = "", val id: Int = 0)
+data class User(val accountName: String = "", val emailAddress: String = "", val password: String = "", val profile: String = "", val id: Int = 0)
--- src/main/resources/db/migration/V1__CreateUserTable.sql
+++ src/main/resources/db/migration/V1__CreateUserTable.sql
@@ -3,5 +3,5 @@ CREATE TABLE user (
     ,account_name VARCHAR(50) NOT NULL
     ,email_address VARCHAR(255) NOT NULL
     ,password VARCHAR(255) NOT NULL
-    ,description VARCHAR (255)
+    ,profile VARCHAR (255)
 )
