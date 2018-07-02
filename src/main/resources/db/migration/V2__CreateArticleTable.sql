4255acb5bb3b8a45d8835b3c07d1087ce025a725
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-07-02 11:21:07 +0900

    テーブルスキーマを用意

--- src/main/resources/db/migration/V2__CreateArticleTable.sql
+++ src/main/resources/db/migration/V2__CreateArticleTable.sql
@@ -1,9 +1,10 @@
-CREATE TABLE user (
+CREATE TABLE article (
 id INT AUTO_INCREMENT NOT NULL PRIMARY KEY
-,account_name VARCHAR(50) NOT NULL UNIQUE
-,email_address VARCHAR(255) NOT NULL UNIQUE
-,password VARCHAR(255) NOT NULL
-,profile VARCHAR (255)
+,title VARCHAR(255) NOT NULL
+,user_id INT NOT NULL
+,released_at	DATETIME	NOT NULL
+,markdown_text TEXT
 ,created_at	timestamp	not null default current_timestamp
 ,updated_at	timestamp	not null default current_timestamp on update current_timestamp
+,FOREIGN KEY(user_id) REFERENCES user(id)
 )
