96b55e4ddc51db542e8df92c09316606bcd97339
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-07-03 14:43:26 +0900

    トレンドページの実装(いいね数は考慮していない)

--- src/main/kotlin/com/final/project/Teechear/mapper/ArticleMapper.kt
+++ src/main/kotlin/com/final/project/Teechear/mapper/ArticleMapper.kt
@@ -9,4 +9,9 @@ interface ArticleMapper {
     fun insert(article: Article)
 
     fun find(id: Int): Article?
+
+    /**
+     * 一週間以内に投稿された記事の中からいいね数が多いものから20件取得する
+     */
+    fun trend(): List<Article>
 }
