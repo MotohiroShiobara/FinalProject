a6b1a9031d3276097f82d08a46d3ebbe7341e64b
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-07-03 11:07:30 +0900

    user_like_articleテーブルを作成

---
+++ src/main/resources/db/migration/V3__CreateUserLikeArticleTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_like_article (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY
+,user_id INT NOT NULL
+,article_id INT NOT NULL
+,FOREIGN KEY(user_id) REFERENCES user (id)
+,FOREIGN KEY(article_id) REFERENCES article (id)
+)
