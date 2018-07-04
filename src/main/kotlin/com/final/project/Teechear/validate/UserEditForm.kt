751984e87fd15e9d515d0ebf4749a63f385d97d9
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-07-04 18:28:30 +0900

    annotationに@get:をつけることでfi

--- src/main/kotlin/com/final/project/Teechear/validate/UserEditForm.kt
+++ src/main/kotlin/com/final/project/Teechear/validate/UserEditForm.kt
@@ -4,8 +4,8 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
 data class UserEditForm(
-        @NotBlank(message = "アカウント名が空です")
-        @Size(min = 4, max = 30, message = "アカウント名は最大4文字以上、30文字以内です")
+        @get:NotBlank(message = "アカウント名が空です")
+        @get:Size(min = 4, max = 30, message = "アカウント名は最大4文字以上、30文字以内です")
         var accountName: String?,
         var profile: String?
 )
