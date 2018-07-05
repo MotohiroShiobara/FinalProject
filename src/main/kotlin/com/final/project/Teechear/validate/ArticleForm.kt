2d3df00b45c86e1c685a439dd0bd929229ce7810
塩原 基弘 <motohiro.shiobara@IT010A12119.local>
2018-07-05 14:00:03 +0900

    他のフォームにもget:を追加

--- src/main/kotlin/com/final/project/Teechear/validate/ArticleForm.kt
+++ src/main/kotlin/com/final/project/Teechear/validate/ArticleForm.kt
@@ -3,6 +3,6 @@ package com.final.project.Teechear.validate
 import javax.validation.constraints.NotBlank
 
 data class ArticleForm (
-    @NotBlank var title: String = "",
+    @get:NotBlank var title: String = "",
     var markdownText: String = ""
 )
