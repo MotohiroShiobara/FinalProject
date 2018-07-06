d78aa5d682aac1f432944f8dc94d4fc1dd13bbd8
塩原 基弘 <motohiro.shiobara@IT010A12119.local>
2018-07-06 14:41:30 +0900

    初期値を入れないとFormの生成時にコンパイルエラーになってしまうためfix

--- src/main/kotlin/com/final/project/Teechear/validate/ArticleForm.kt
+++ src/main/kotlin/com/final/project/Teechear/validate/ArticleForm.kt
@@ -3,6 +3,6 @@ package com.final.project.Teechear.validate
 import javax.validation.constraints.NotBlank
 
 data class ArticleForm (
-    @get:NotBlank val title: String?,
-    val markdownText: String?
+    @get:NotBlank val title: String = "",
+    val markdownText: String = ""
 )
