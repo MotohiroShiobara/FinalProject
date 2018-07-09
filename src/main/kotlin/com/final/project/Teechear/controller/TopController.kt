95ed9ee9953bb23bbc479a0626ad3392180a5245
塩原 基弘 <motohiro.shiobara@IT010A12119.local>
2018-07-09 15:12:26 +0900

    投稿物に

--- src/main/kotlin/com/final/project/Teechear/controller/TopController.kt
+++ src/main/kotlin/com/final/project/Teechear/controller/TopController.kt
@@ -1,16 +1,20 @@
 package com.final.project.Teechear.controller
 
 import com.final.project.Teechear.mapper.ArticleMapper
+import com.final.project.Teechear.mapper.UserMapper
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import java.nio.file.attribute.GroupPrincipal
+import java.security.Principal
 
 @Controller
-class TopController(private val articleMapper: ArticleMapper) {
+class TopController(private val articleMapper: ArticleMapper, private val userMapper: UserMapper) {
 
     @GetMapping("/trend")
-    fun trend(model: Model): String {
+    fun trend(model: Model, principal: Principal): String {
         model.addAttribute("articleList", articleMapper.trend())
+        model.addAttribute("currentUserId", userMapper.findByEmailOrName(principal.name).id)
         return "top/trend"
     }
 }
