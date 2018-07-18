406ffda79264fb3d4bb25e9b2150f4559c880e74
塩原 基弘 <motohiro.shiobara@IT010A12119.local>
2018-07-18 17:34:36 +0900

    LocalDateTime型をDate型に変換するためのserviceクラスを実装

--- src/main/kotlin/com/final/project/Teechear/service/LessonService.kt
+++ src/main/kotlin/com/final/project/Teechear/service/LessonService.kt
@@ -2,11 +2,18 @@ package com.final.project.Teechear.service
 
 import com.final.project.Teechear.validate.LessonNewForm
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
-class LessonService {
+class LessonService(
+        private val dateTimeService: DateTimeService
+) {
 
     fun createByForm(lessonNewForm: LessonNewForm) {
-        println(lessonNewForm)
+        val eventDateTime = lessonNewForm.eventDateTime
+        if (eventDateTime is LocalDateTime) {
+        }
+
     }
+
 }
