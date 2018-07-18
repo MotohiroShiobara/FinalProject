eefef95668046521edbd6e2a8cd8968a1661d2dd
塩原 基弘 <motohiro.shiobara@IT010A12119.local>
2018-07-18 17:09:49 +0900

    entityクラスの実装

---
+++ src/main/kotlin/com/final/project/Teechear/entity/LessonEntity.kt
@@ -0,0 +1,14 @@
+package com.final.project.Teechear.entity
+
+import java.util.*
+
+data class LessonEntity(
+        val title: String? = null,
+        val eventDateTime: Date? = null,
+        val price: Int? = null,
+        val description: String? = null,
+        val imageUrl: String? = null,
+        val emailAddress: String? = null,
+        val ownerId: Int? = null,
+        val isOpen: Boolean? = null
+)
---
+++ src/main/kotlin/com/final/project/Teechear/entity/UserApplyLessonEntity.kt
@@ -0,0 +1,9 @@
+package com.final.project.Teechear.entity
+
+import java.util.*
+
+data class UserApplyLessonEntity(
+        val lessonId: Int? = null,
+        val userId: Int? = null,
+        val applyDateTime: Date? = null
+)
