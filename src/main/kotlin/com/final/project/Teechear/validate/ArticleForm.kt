d9a17db935a2b8876cbed9da8c6cb1655d2bc61c
塩原 基弘 <motohiro.shiobara@BIZ0015000399.local>
2018-07-20 17:20:29 +0900

    デザインを変更

--- src/main/kotlin/com/final/project/Teechear/validate/ArticleForm.kt
+++ src/main/kotlin/com/final/project/Teechear/validate/ArticleForm.kt
@@ -1,9 +1,11 @@
 package com.final.project.Teechear.validate
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class ArticleForm (
     @get:NotBlank(message = "必須項目です")
+    @get:Size(min = 1, max = 50, message = "1文字以上50文字以下で入力してください")
     val title: String = "",
     @get:NotBlank(message = "必須項目です")
     val markdownText: String = ""
