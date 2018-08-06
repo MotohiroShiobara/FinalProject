59142fee53eb796cef1021cbc59a1371e83632f1
塩原 基弘 <motohiro.shiobara@IT010A12119.local>
2018-08-06 11:46:39 +0900

    exceptionパッケージ以下のクラスにjavadocを追加

--- src/main/kotlin/com/final/project/Teechear/exception/DomainArgumentException.kt
+++ src/main/kotlin/com/final/project/Teechear/exception/DomainArgumentException.kt
@@ -2,5 +2,7 @@ package com.final.project.Teechear.exception
 
 /**
  * EntityからDomainに変換する際に必要なカラムが不足している場合の例外クラス
+ * @param message ログに出力するためのメッセージ
+ * TODO messageを出力するためのLoggerを実装する
  */
-class DomainArgumentException : Exception()
+class DomainArgumentException(message: String) : Exception()
