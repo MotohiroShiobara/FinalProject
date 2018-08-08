3047684f8625d8b77fae4f6b1e00a3d68a2fa1f5
MotohiroShiobara <motohiro.shiobara@bizreach.co.jp>
2018-08-08 21:07:47 +0900

    不要なものを削除

--- src/main/kotlin/com/final/project/Teechear/service/ArticleLikeService.kt
+++ src/main/kotlin/com/final/project/Teechear/service/ArticleLikeService.kt
@@ -1,11 +1,17 @@
 package com.final.project.Teechear.service
 
+import com.final.project.Teechear.domain.User
 import com.final.project.Teechear.entity.UserLikeArticleEntity
+import com.final.project.Teechear.mapper.ArticleMapper
 import com.final.project.Teechear.mapper.UserLikeArticleMapper
+import com.final.project.Teechear.mapper.UserMapper
 import org.springframework.stereotype.Service
+import java.security.Principal
 
 @Service
-class ArticleLikeService(private val userLikeArticleMapper: UserLikeArticleMapper) {
+class ArticleLikeService(private val userLikeArticleMapper: UserLikeArticleMapper,
+                         private val userMapper: UserMapper,
+                         private val articleMapper: ArticleMapper) {
 
     fun likeCount(articleId: Int): Int {
         return userLikeArticleMapper.articleLikeCount(articleId)
@@ -13,5 +19,31 @@ class ArticleLikeService(private val userLikeArticleMapper: UserLikeArticleMappe
 
     fun isLikedByUser(articleId: Int, currentUserId: Int): Boolean {
         return userLikeArticleMapper.findByUserIdAndArticleId(articleId, currentUserId) is UserLikeArticleEntity
+    }
+
+    fun create(articleId: Int, principal: Principal) {
+        val currentUser = userMapper.findByEmailOrName(principal.name)
+        val currentUserId = currentUser?.id!!
+        if (validation(articleId, currentUserId)) {
+            val like = UserLikeArticleEntity(currentUserId, articleId)
+            userLikeArticleMapper.insert(like)
+        }
+    }
+
+    fun delete(articleId: Int, user: User) {
+        userLikeArticleMapper.delete(articleId, user.id)
+    }
+
+    private fun validation(articleId: Int, userId: Int):Boolean {
+        if (userLikeArticleMapper.findByUserIdAndArticleId(articleId, userId) is UserLikeArticleEntity) {
+            return false
+        }
+
+        // そのArticleのuser_idがいいねしたユーザーの場合、無効にする
+        if (articleMapper.find(articleId)?.userId == userId) {
+            return false
+        }
+
+        return true
     }
 }
